9ccf4b4dc6593c48f4a298bf93d938b1b389873c
Dave Syer <dsyer@gopivotal.com>
2015-01-27 13:23:08 +0000

    Simplify layout of deck.js resources

--- _plugins/readme.rb
+++ _plugins/readme.rb
@@ -1,16 +1,2 @@
 puts "Copying README"
 FileUtils.copy 'README.md', '_includes/README.md'
-
-module Jekyll
-
-  class FencedHighlightGenerator < Generator
-    safe true
-    def generate(site)
-      source = site.source
-      site.source = 'deck.js'
-      site.read_directories
-      site.source = source
-    end
-  end
-
-end
