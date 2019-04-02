32d1b0dc38b8aa3ce7fc524f11055aec1ab2280b
Dave Syer <dsyer@pivotal.io>
2019-04-02 15:24:31 +0100

    Upgrade to asciidoctor 2, plus add diagram support

--- _plugins/preprocess.rb
+++ _plugins/preprocess.rb
@@ -5,18 +5,16 @@ require 'haml'
 
 puts "Copying README"
 FileUtils.copy 'README.md', '_includes/README.md'
+Asciidoctor::DEFAULT_EXTENSIONS['deckjs'] = '.html'
 
 module Jekyll
 
   class FencedHighlightGenerator < Generator
-    safe true
     def generate(site)
-      options = {:mkdirs => true, :safe => :unsafe, :attributes => 'linkcss', :to_dir => '_site/decks', :template_dir => 'asciidoctor-backends/haml/deckjs'}
+      options = {:mkdirs => true, :safe => :unsafe, :attributes => 'linkcss', :to_dir => '_site/decks', :template_dirs => ['asciidoctor-backends/haml/deckjs'], :to_file => true}
       Dir.glob('decks/*.adoc') do |fname|
-        output = (File.basename(fname, '.adoc') + '.html')
-        site.keep_files << output
-        puts 'Rendering ' + fname + ' to ' + output
-        Asciidoctor.render_file(fname, options)
+        puts 'Rendering ' + fname
+        Asciidoctor.convert_file(fname, options)
       end
       FileUtils.cp_r 'deck.js/', '_site/'
       FileUtils.cp_r 'themes/', '_site/deck.js/'
